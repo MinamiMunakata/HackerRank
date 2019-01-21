e577934e2ead29860fb33c7b87bf384b711ba032
Minami Munakata <june28munazo@gmail.com>
2019-01-20 17:04:59 -0800

    Modify day3

--- Day03.java
+++ Day03.java
@@ -1,17 +1,11 @@
-import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
 import java.util.*;
-import java.util.concurrent.*;
-import java.util.regex.*;
 
 public class Day03 {
     private static String weirdOrNot(int n) {
         if (n % 2 == 0) {
             if (n > 20)
                 return "Not Weird";
-            if (n >= 2 && 2 <= 5)
+            if (n >= 2 && n <= 5)
                 return "Not Weird";
         }
         return "Weird";
@@ -24,6 +18,7 @@ public class Day03 {
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
         scanner.close();
+
         System.out.println(weirdOrNot(N));
     }
 }
